4b365088cfbb5019291c33b6e4f315069ef47a7f
Jerry Adriano <jerry.adriano@liferay.com>
2024-05-02 13:24:17 -0300

    Exercise2

---
+++ Exercise2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.Scanner;
+public class Exercise2 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um numero");
+        int a = scanner.nextInt();
+        System.out.println("Digite um numero");
+        int b = scanner.nextInt();
+        System.out.println("Digite um numero");
+        int c = scanner.nextInt();
+        System.out.println(a + b + c / 3);
+    }
+}
