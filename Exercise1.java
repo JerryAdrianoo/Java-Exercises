2e42675e98a4e5469855a2cc22f5f88b4f9b28df
Jerry Adriano <jerry.adriano@liferay.com>
2024-05-02 13:16:35 -0300

    Exercise1

---
+++ Exercise1.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> number = new ArrayList<>();
+        for (int i = 0; i < 3; i++){
+            System.out.println("Type the Number :");
+            int output = scanner.nextInt();
+            number.add(output);
+        }
+        Exercise1 exercise1 = new Exercise1();
+        exercise1.findNumber(number);
+    }
+    public static void findNumber(ArrayList<Integer> test){
+        test.sort(null);
+        System.out.println("\n" + test.get(0));
+    }
+
+//    1. Write a Java method to find the smallest number among three numbers.
+//    Test Data:
+//    Input the first number: 25
+//    Input the Second number: 37
+//    Input the third number: 29
+//    Expected Output:
+//
+//    The smallest value is 25.0
+}
