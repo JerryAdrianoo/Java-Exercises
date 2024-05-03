e75fe26cea37ccfc8cc1e93fe80f970b04dd80bc
Jerry Adriano <jerry.adriano@liferay.com>
2024-05-03 15:28:51 -0300

    Exercise 3

---
+++ Exercise3.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+public class Exercise3 {
+public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("test");
+        String numero = scanner.nextLine();
+        System.out.println(numero.substring(1, 2));
+}
+
+
+//    Write a Java method to display the middle character of a string.
+//    Note: a) If the length of the string is odd there will be two middle characters.
+//    b) If the length of the string is even there will be one middle character.
+//    Test Data:
+//    Input a string: 350
+//    Expected Output:
+//
+//
+//    The middle character in the string: 5
+}
