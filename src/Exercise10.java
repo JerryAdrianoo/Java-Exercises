f777821d65ae8a9afa2ae359e1b97ea34fbed3d8
Jerry Adriano <jerry.adriano@liferay.com>
2024-05-13 11:07:35 -0300

    Exercise 10

---
+++ src/Exercise10.java
@@ -0,0 +1,28 @@
+public class Exercise10 {
+    public static void main(String[] args) {
+        int year = 2020;
+
+        calcularAno(year);
+    }
+
+    public static boolean calcularAno(int year) {
+        boolean bi = false;
+
+        if (year % 4 == 0 || year % 400 == 0) {
+            bi = true;
+            System.out.println("esse é um ano bissexto!\n" + bi);
+        }
+        else if ((year % 4 != 0 || year % 100 == 0)){
+            System.out.println("Esse não é um ano bissexto!\n" + bi);
+        }
+
+        return bi;
+    }
+}
+
+//10. Write a Java method to check whether a year (integer) entered by the user is a leap year or not.
+//
+//Expected Output:
+//
+//Input a year: 2017
+//false
