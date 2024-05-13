d83443676d82ffed2475dec44f553f5bbcc94ee3
Jerry Adriano <jerry.adriano@liferay.com>
2024-05-13 11:18:33 -0300

    Exercise 10 fix

--- src/Exercise10.java
+++ src/Exercise10.java
@@ -1,6 +1,6 @@
 public class Exercise10 {
     public static void main(String[] args) {
-        int year = 2020;
+        int year = 2024;
 
         calcularAno(year);
     }
@@ -8,12 +8,13 @@ public class Exercise10 {
     public static boolean calcularAno(int year) {
         boolean bi = false;
 
-        if (year % 4 == 0 || year % 400 == 0) {
+        if ((year % 4 != 0 || year % 100 == 0)){
+            System.out.println("Esse não é um ano bissexto!\n" + bi);
+
+        }
+        else if (year % 4 == 0 || year % 400 == 0) {
             bi = true;
             System.out.println("esse é um ano bissexto!\n" + bi);
-        }
-        else if ((year % 4 != 0 || year % 100 == 0)){
-            System.out.println("Esse não é um ano bissexto!\n" + bi);
         }
 
         return bi;
